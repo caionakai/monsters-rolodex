a092686331a66405cdefe156641d431cb54cb812
caionakai <kaioesperto@hotmail.com>
2019-08-29 17:09:34 -0300

    Updates

---
+++ precache-manifest.24bc51042274ed79e09cdc5ab5f63e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4220b5d9a61fc0c1cd7b113237291a6f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eacc10f2a827d6704e53",
+    "url": "/monsters-rolodex/static/css/main.868d15fe.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "eacc10f2a827d6704e53",
+    "url": "/monsters-rolodex/static/js/main.6c5098c9.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
